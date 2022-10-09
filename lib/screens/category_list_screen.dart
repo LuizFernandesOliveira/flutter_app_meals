302caf732d90c33bf6c2eddeee68ae5f0f60d9b2
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2022-10-09 18:17:57 -0300

    add categories

--- lib/screens/category_list_screen.dart
+++ lib/screens/category_list_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
 
 class CategoryListScreen extends StatelessWidget {
+  final String title = 'Vamos cozinhar?';
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [],
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(title)),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(category: category);
+        }).toList(),
+      ),
     );
   }
 }
