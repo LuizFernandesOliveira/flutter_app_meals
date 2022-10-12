06f12de4ddbc8b4df8ebcd56677b7812215945bc
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2022-10-12 01:27:10 -0300

    feat: add meal item

--- lib/screens/meal_list_screen.dart
+++ lib/screens/meal_list_screen.dart
@@ -1,18 +1,28 @@
+import 'package:app_meals/components/meal_item.dart';
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class MealListScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final meals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            meal: meals[index],
+          );
+        },
       ),
     );
   }
